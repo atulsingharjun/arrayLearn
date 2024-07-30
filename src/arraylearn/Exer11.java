5a07da181cccaa4a418b6e79c8a334bfb64ee2db
Atul <atulksingh.15@gmail.com>
2024-07-30 20:16:49 +0530

    leap

--- src/arraylearn/Exer11.java
+++ src/arraylearn/Exer11.java
@@ -5,31 +5,27 @@ import java.util.Scanner;
 public class Exer11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int count,N,total=0;
-//		Scanner sc = new Scanner(System.in);
-//		System.out.println("enter a no ");
-//		N=sc.nextInt()	;	
-		
-		for(int i=2;i<=10 ;i++) {
-			count=1;
-			for(int j=1;j<=i;j++) {
-				
-				if(i%j == 0) {  
-					count++;
-					
-				}
-				
-				if(count == 2) {  //yha hm factor dikte hai ya to 1 se kate ya khud se isliye 2 ka factor ho
-					System.out.println(i);
-					//total=total+1;
-				
-				}
-				
-			}
-			
-		}
-		//System.out.println("total no" +total);
-	}
+        int N=10;
+       
+        
+        for(int i=1;i<=N;i++) {
+            int count=0;
+            for(int j=1;j<=i;j++) {
+                
+                if(i%j==0) 
+                count++;
+                
+                
+                   
+                }
+            if(count==2) {
+                   
+                   System.out.println(i);
+               } 
+            }
+       
+            
+        }
+       
 
 }
