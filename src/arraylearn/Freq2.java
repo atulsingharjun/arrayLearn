bbada57af7a9c0122023e1e59937f19068573442
Atul <atulksingh.15@gmail.com>
2024-08-07 20:12:23 +0530

    2 program 7 aug

--- src/arraylearn/Freq2.java
+++ src/arraylearn/Freq2.java
@@ -5,8 +5,16 @@ public class Freq2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a[]= {7,4,7,8,3,4,8};
-		int n =a.length;
-		
+		for(int i=0;i<a.length;i++	) {	
+			int x=a[i];
+			int count =0;
+			for(int j=0;j<a.length;j++) {
+				if(a[j]==x) {
+					count++;
+				}
+			}
+			System.out.println("freq of "+x+ "is" +count);
+		}
 
 	}
 
